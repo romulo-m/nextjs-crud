49be0b1135a7c2cf3e159f3ffd094538bd6f262e
Broad <romulo.mgaudencio@gmail.com>
2022-08-29 18:23:46 -0300

    Estilização da tabela e lógica inicial implement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,37 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 15, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Gabriel', 54, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+      console.log(cliente.nome);
+      
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(cliente.nome);
+    
+}
+
   return (
     <div className={`
       flex h-screen justify-center items-center
-      bg-gradient-to-r from-purple-500 to-blue-600
+      bg-gradient-to-r from-blue-600 to-purple-500
       text-white
     `}>
      <Layout titulo="Cadastro de Usuários">
-      <span>Conteudo</span>
+      <Tabela clientes={clientes} 
+      clienteSelecionado={clienteSelecionado} 
+      clienteExcluido={clienteExcluido}      
+      />
      </Layout>
     </div>
   )
